db304b4e2b7752d16a7fe08e2b6e48ac25dae4c5
AlexanderBaikal <abaikaler@mail.ru>
2021-07-10 21:29:40 +0800

    started working with YT-API

---
+++ src/redux/actions/videos.action.js
@@ -0,0 +1,40 @@
+import {
+  HOME_VIDEOS_FAIL,
+  HOME_VIDEOS_REQUEST,
+  HOME_VIDEOS_SUCCESS,
+} from "../actionType";
+
+import request from "../../api";
+
+export const getPopularVideos = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: HOME_VIDEOS_REQUEST,
+    });
+    const { data } = await request("/videos", {
+      params: {
+        part: "snippet, contentDetails,statistics",
+        chart: "mostPopular",
+        regionCode: "RU",
+        maxResults: 20, 
+        pagetoken: "",
+      },
+    });
+
+    dispatch({
+      type: HOME_VIDEOS_SUCCESS,
+      payload: {
+        videos: data.items,
+        nextPageToken: data.nextPageToken,
+      },
+    });
+
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+    dispatch({
+      type: HOME_VIDEOS_FAIL,
+      payload: error.message,
+    });
+  }
+};
--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import CategoriesBar from "../../components/categoriesBar/CategoriesBar";
+import { getPopularVideos } from "../../redux/actions/videos.action";
 import Video from "./../../components/video/Video";
 
 function HomeScreen() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPopularVideos());
+  }, [dispatch]);
+
+  const { videos } = useSelector((state) => state.homeVideos);
+
   return (
     <Container>
       <CategoriesBar />
       <Row>
-        {[...new Array(20).keys()].map((i) => (
-          <Col lg={3} md={4} key={i}>
-            <Video />
+        {videos.map((video) => (
+          <Col lg={3} md={4}>
+            <Video video={video} key={video.id} />
           </Col>
         ))}
       </Row>
